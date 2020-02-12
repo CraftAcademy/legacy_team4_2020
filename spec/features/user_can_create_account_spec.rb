112438c5741c0483bacecc0e8f265565c558383f
Jaime <jaime.cruz.hostalot@gmail.com>
2020-02-12 22:10:22 +0100

    added sad and happy path, forgot to push earlier, sorry

--- spec/features/user_can_create_account_spec.rb
+++ spec/features/user_can_create_account_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+
 feature 'Check if user can create account' do
   context 'user is in sign up page.'
 
@@ -25,5 +26,53 @@ feature 'Check if user can create account' do
 
   it 'User can see log in, in sign up' do
     expect(page).to have_content 'Log in'
+  end
+end
+
+# Filling form and making test:
+
+feature 'User can create the account' do
+  
+  before do
+    visit new_user_registration_path
+  end
+
+  describe "successfully" do
+    before do
+
+      fill_in 'Name', with: "jaime"
+      fill_in 'Email', with: "asd@gmail.com"
+      fill_in 'Password', with: "asdasd12"
+      fill_in 'Password confirmation', with: "asdasd12"
+
+      click_on 'Create'
+    end
+
+    it 'Welcome message gets displayed' do
+      expect(page).to have_content 'Welcome! You have signed up successfully.'
+    end
+
+    it 'sends user to index page' do
+      expect(current_path).to eq root_path
+    end
+  end
+
+  describe 'Sad path, user forgets email' do
+
+    describe 'invalid email' do
+        before do
+            fill_in 'Name', with: 'jaime'
+            fill_in 'Email', with: 'asdgmail.com'
+            fill_in 'Password', with: 'asdasd12'
+            fill_in 'Password confirmation', with: 'asdasd12'
+    
+    
+            click_on 'Create'
+        end
+
+        it 'returns the email is invalid' do
+            expect(page).to have_content 'Email is invalid'
+        end
+    end
   end
 end
